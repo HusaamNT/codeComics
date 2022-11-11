d4c99e5c70457fdf5168593ac641bea681378a64
Husaam NT <husnaeem@gmail.com>
2022-11-11 16:45:02 +0000

    search function added 1st ver

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,29 @@
-let characterName=document.getElementById("character-name")
-let characterNameDisplay=""
-let characterDiscription=document.getElementById("character-description")
-let characterDiscriptionDisplay=""
-let characterPhoto=document.getElementById("character-photo")
-let characterPhotoLink=""
+let characterName = document.getElementById("character-name");
+let characterNameDisplay = "";
+let characterDiscription = document.getElementById("character-description");
+let characterDiscriptionDisplay = "";
+let characterPhoto = document.getElementById("character-photo");
+let characterPhotoLink = "";
 
-let searchBtn=document.getElementById("search-button")
-let characterInput=document.getElementById("search-box");
+let searchBtn = document.getElementById("search-button");
+let characterInput = document.getElementById("search-box");
 
-//const searchBarInput2=searchBarInput.value
-//searchBtn.addEventListener("click", search)
-//const search = function(){
-//  const marvelInput = characterInput.textContent
-//}
-const marvelInput= "spid"
+const search = function(){
+  marvel()
+  reddit()
+}
 
-const marvelInputNoS = marvelInput.replace(/\s+/g, '');
+// const marvelInput = characterInput.value
 
-const marvelInputNoD = marvelInputNoS.replace(/-/g, '');
-const marvelCharacter = marvelInputNoD.toLowerCase()
+// console.log(marvelInput)
+
+searchBtn.addEventListener("click", search)
+
+//const marvelInput = "spid";
+
+// const marvelInputNoS = marvelInput.replace(/\s+/g, "");
+// const marvelInputNoD = marvelInputNoS.replace(/-/g, "");
+// const marvelCharacter = marvelInputNoD.toLowerCase();
 
 //alert("js starting point work")
 // function search(event){
@@ -33,20 +38,30 @@ const marvelCharacter = marvelInputNoD.toLowerCase()
 //https://gateway.marvel.com/v1/public/characters?nameStartsWith="+marvelCharacter+"&ts=1&apikey=c6c410f564a7361717294de109f25d9a&hash=bb8d62d7bf94d0ca3c9a989e86a12dda")
 //;/v1/public/characters/
 
-async function marvel(){
-    const response =await fetch("https://gateway.marvel.com/v1/public/characters?nameStartsWith="+marvelInputNoS+"&limit=100&ts=1&apikey=c6c410f564a7361717294de109f25d9a&hash=bb8d62d7bf94d0ca3c9a989e86a12dda");
-    const data = await response.json()  
-    const name_loop=data.data.count
-    console.log(data)
-     console.log("name_loop is "+name_loop)
-      for(i=0;i<name_loop-1;i++){
-        const characterName = (data.data.results[i].name)
-        const characterImage = (data.data.results[i].thumbnail.path+".jpg")
-        const characterDescription = (data.data.results[i].description)
-        console.log(characterImage)
+async function marvel() {
+  const marvelInput = characterInput.value
+
+  console.log(marvelInput)
+  const marvelInputNoS = marvelInput.replace(/\s+/g, "");
+
+  const response = await fetch(
+    "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" +
+      marvelInputNoS +
+      "&limit=100&ts=1&apikey=c6c410f564a7361717294de109f25d9a&hash=bb8d62d7bf94d0ca3c9a989e86a12dda"
+  );
+  const data = await response.json();
+  const name_loop = data.data.count;
+  console.log(data);
+  console.log("name_loop is " + name_loop);
+  for (i = 0; i < name_loop - 1; i++) {
+    const characterName = data.data.results[i].name;
+    const characterImage = data.data.results[i].thumbnail.path + ".jpg";
+    const characterDescription = data.data.results[i].description;
+    console.log(characterImage);
+
+    const headingElement = $(`
         
-        const headingElement = $(`
-        
+        <div class="columns pt-6 is-flex-direction-row">
         <div class="columns pt-6">
         <div class="column">
           <div class="card">
@@ -70,93 +85,100 @@ async function marvel(){
           </div>
         </div>
         </div>
+        </div>
 
 `);
 
-$("#card-container").append(headingElement);
+    $("#card-container").append(headingElement);
+  }
 
-}
+  //console.log("name_for_loop runs normally")
+  //console.log(data.data.results[i])
 
-        //console.log("name_for_loop runs normally")
-        //console.log(data.data.results[i])
+  // characterNameDisplay=[]
+  // characterNameDisplay[i]=document.createElement("h1")
+  // characterNameDisplay[i]=data.data.results[i].name
+  // characterName.append(characterNameDisplay)
 
-        // characterNameDisplay=[]
-        // characterNameDisplay[i]=document.createElement("h1")
-        // characterNameDisplay[i]=data.data.results[i].name
-        // characterName.append(characterNameDisplay)
+  // characterPhotoLink=[]
+  // characterPhotoLink[0]=document.createElement("p")
+  // characterPhotoLink[0]=data.data.results[0].thumbnail.path
+  // photo1 =  window.location.href="characterPhotoLink[0]"
+  // characterPhoto.append(characterPhotoLink)
 
-        // characterPhotoLink=[]
-        // characterPhotoLink[0]=document.createElement("p")
-        // characterPhotoLink[0]=data.data.results[0].thumbnail.path
-        // photo1 =  window.location.href="characterPhotoLink[0]" 
-        // characterPhoto.append(characterPhotoLink)
-       
-
-
-        /*characterDiscriptionDisplay=[]
+  /*characterDiscriptionDisplay=[]
         characterDiscriptionDisplay[i]=document.createElement("h2")
         characterDiscriptionDisplay[i]=data.data.results[i].description
         characterDiscription.append(characterDiscriptionDisplay)*/
 
-       
-
-        // console.log("characterNameDisplay: "+characterNameDisplay)
-        // console.log("name_for_loop runs normally")
-        
-      
-        
-        
-
-};
-marvel()
-
-async function redditMarvel(){ //fetching 20 hot posts from the characters searched subreddit
-    const response = await fetch ("https://oauth.reddit.com/r/marvel/hot.json?limit=20",{ //change to 100 if using the search for loop
-      headers: {  
-        Authorization: "bearer 675480154540-RRiPTLQ2q6qgcWVIFYxl8hjPimu-AQ"
-        }
-})
-const data = await response.json()
-    const children = (data.data.children.length)
-    console.log(data)
-    for (i=0; i < children; i++){
-        const marvelTitle = (data.data.children[i].data.title)
-        const marvelText = (data.data.children[i].data.selftext)
-        const marvelCommentCount = (data.data.children[i].data.num_comments)
-        const marvelImageURL = (data.data.children[i].data.thumbnail)
-        const marvelPerma = (data.data.children[i].data.permalink)
-        const marvelLink = ("https://www.reddit.com"+marvelPerma)
-        console.log(marvelTitle)
-        console.log(marvelCommentCount)
-        console.log(marvelText)
-        console.log(marvelImageURL)
-        console.log(marvelLink)
+  // console.log("characterNameDisplay: "+characterNameDisplay)
+  // console.log("name_for_loop runs normally")
 }
-};
 
-async function reddit(){ //fetching 20 hot posts from the characters searched subreddit
-    const response = await fetch ("https://oauth.reddit.com/r/"+marvelCharacter+"/hot.json?limit=20",{ //change to 100 if using the search for loop
-      headers: {  
-        Authorization: "bearer 675480154540-RRiPTLQ2q6qgcWVIFYxl8hjPimu-AQ"
-      }
-    })
-    const data = await response.json()
-    const children = (data.data.children.length)
-    console.log(data)
-    for (i=0; i < children; i++){
-        const characterTitleR = (data.data.children[i].data.title)
-        const characterTextR = (data.data.children[i].data.selftext)
-        const characterCommentCountR = (data.data.children[i].data.num_comments)
-        const characterImageURLR = (data.data.children[i].data.thumbnail)
-        const characterPermaR = (data.data.children[i].data.permalink)
-        const characterLinkR = ("https://www.reddit.com"+characterPermaR)
-        console.log(characterTitleR)
-        console.log(characterCommentCountR)
-        console.log(characterTextR)
-        console.log(characterImageURLR)
-        console.log(characterLinkR)
-        
-        const headingElement = $(`
+
+async function redditMarvel() {
+  //fetching 20 hot posts from the characters searched subreddit
+  const response = await fetch(
+    "https://oauth.reddit.com/r/marvel/hot.json?limit=20",
+    {
+      //change to 100 if using the search for loop
+      headers: {
+        Authorization: "bearer 675480154540-QX6bmNuDFuuK7-TpeNuJDwXt1_hmdg",
+      },
+    }
+  );
+  const data = await response.json();
+  const children = data.data.children.length;
+  console.log(data);
+  for (i = 0; i < children; i++) {
+    const marvelTitle = data.data.children[i].data.title;
+    const marvelText = data.data.children[i].data.selftext;
+    const marvelCommentCount = data.data.children[i].data.num_comments;
+    const marvelImageURL = data.data.children[i].data.thumbnail;
+    const marvelPerma = data.data.children[i].data.permalink;
+    const marvelLink = "https://www.reddit.com" + marvelPerma;
+    console.log(marvelTitle);
+    console.log(marvelCommentCount);
+    console.log(marvelText);
+    console.log(marvelImageURL);
+    console.log(marvelLink);
+  }
+}
+
+async function reddit() {
+  const marvelInput = characterInput.value
+
+  const marvelInputNoS = marvelInput.replace(/\s+/g, "");
+  const marvelInputNoD = marvelInputNoS.replace(/-/g, "");
+  const marvelCharacter = marvelInputNoD.toLowerCase();
+
+  //fetching 20 hot posts from the characters searched subreddit
+  const response = await fetch(
+    "https://oauth.reddit.com/r/" + marvelCharacter + "/hot.json?limit=20",
+    {
+      //change to 100 if using the search for loop
+      headers: {
+        Authorization: "bearer 675480154540-RRiPTLQ2q6qgcWVIFYxl8hjPimu-AQ",
+      },
+    }
+  );
+  const data = await response.json();
+  const children = data.data.children.length;
+  console.log(data);
+  for (i = 0; i < children; i++) {
+    const characterTitleR = data.data.children[i].data.title;
+    const characterTextR = data.data.children[i].data.selftext;
+    const characterCommentCountR = data.data.children[i].data.num_comments;
+    const characterImageURLR = data.data.children[i].data.thumbnail;
+    const characterPermaR = data.data.children[i].data.permalink;
+    const characterLinkR = "https://www.reddit.com" + characterPermaR;
+    console.log(characterTitleR);
+    console.log(characterCommentCountR);
+    console.log(characterTextR);
+    console.log(characterImageURLR);
+    console.log(characterLinkR);
+
+    const headingElement = $(`
         <article class="media" id="redditPost3">
         <figure class="media-left">
           <p class="image is-64x64">
@@ -190,10 +212,9 @@ async function reddit(){ //fetching 20 hot posts from the characters searched su
         </div>
       </article>
 `);
-$("#reddit").append(headingElement);
+    $("#reddit").append(headingElement);
 
-
-        /*if (marvelText===""){
+    /*if (marvelText===""){
             continue
         }
         if (marvelTitle.includes(marvelCharacter) || marvelText.includes(marvelCharacter)){
@@ -204,7 +225,10 @@ $("#reddit").append(headingElement);
             console.log("No matches")
         }
     }*/
-}}
+  }
+}
 
-//const cardInfo = 
-reddit()
+//const cardInfo =
+
+//reddit();
+//marvel()
